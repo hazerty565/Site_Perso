134d6092c9f4b6a070598ba850d8b2c8de3cd9cf
hazerty565 <hazerty565@hotmail.com>
2022-02-10 15:31:43 +0100

    change

---
+++ components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+ <header class="fixed  ml-1 bg-gray-700 text-white text-center relative">
+  <NuxtLink to="/">Home Page</NuxtLink> /
+  <NuxtLink to="/test">Test Page</NuxtLink>
+      </header>
+</template>
+
+<script>
+//import Logo from "@/components/Logo";
+export default {
+  name: "nav-bar",
+};
+</script>
